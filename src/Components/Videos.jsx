f517006e536af0dc0339ca72c77ba62acc84e66d
Emmah-oo <emmaochonogor715@gmail.com>
2023-04-06 19:34:56 +0100

    fixed err with data fetch

--- src/Components/Videos.jsx
+++ src/Components/Videos.jsx
@@ -3,14 +3,14 @@ import Videocard from './Videocard'
 import ChannelCard from './ChannelCard'
 
 const Videos = ({ videos }) => {
-    console.log(videos)
+  console.log(videos)
     
   return (
     <div className='grid sm:grid-cols-2 lg:grid-cols-3 gap-2 p-2'>
-      {videos.map((video, idx) => (
+      {videos?.map((video, idx) => (
         <div key={idx} className='w-[100%]'>
-            {video.id.videoId && <Videocard video = {video}/>}
-            {video.id.channelId && <ChannelCard />}
+          {video.id.videoId && <Videocard video = {video}/>}
+          {video.id.channelId && <ChannelCard />}
         </div>
       ))}
     </div>
